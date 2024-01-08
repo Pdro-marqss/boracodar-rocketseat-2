7f71619c94e8d8753cae4414b54f426672cae8fd
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-01-08 15:49:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section id="products-page">
+    <div id="left-side"></div>
+    <div id="right-side"></div>
+  </section>
+</template>
+
+<style scoped>
+#products-page {
+  height: 100dvh;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+#left-side {
+  background-color: red;
+}
+</style>
